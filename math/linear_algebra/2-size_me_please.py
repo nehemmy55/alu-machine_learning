b5695aa4896b61bca5c30163e90856f4f8c40360
Nehemie ishimwe <n.ishimwe5@alustudent.com>
2026-01-23 21:46:43 +0200

    DONE
    Refactor matrix_shape function to return full shape of a matrix.

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
-
-
 def matrix_shape(matrix):
-    ncol = len(matrix[0])
-    nrow = len(matrix)
-    return (nrow, ncol)
+    """RETURN SHAPE 
+    """
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        if len(matrix) == 0:
+            break
+        matrix = matrix[0]
+    return shape
